acb0d7161d12fdb282a837e2f03ab86ecbeba061
GitHub Action <action@github.com>
2024-03-15 17:36:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/lylanv/greenedge/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lylanv/greenedge/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lylanv.greenedge.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
